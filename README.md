db93d5898316c9edc4e7c5e8a076f474b022e70d
Felix <burrfelix@aol.de>
2023-07-10 15:25:26 +0200

    feature engineering

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # tron
 SDL2 tron implementation with a python wrapper.
-Supports up to 8 players on a 20x24 grid
+Supports up to 8 players on a 20x24 grid.
--- tron.py
+++ tron.py
@@ -9,6 +9,7 @@ import torch.nn as nn
 import random
 import neat
 import visualize
+import skimage
 
 libname = pathlib.Path().absolute() / "libtron.so"
 tron = ctypes.CDLL(libname)
@@ -69,10 +70,25 @@ def get_distances(x,y):
     while(playfield[(y-down+20)%20,x] == 0xFF):
         down = down + 1
     return [up, left, down, right]
-        
+
+def floodfill(x, y, field):
+    x = (x+24)%24
+    y = (y+20)%20
+    result = 0
+    if field[y,x] == 0:
+        mask = skimage.segmentation.flood(field, (y,x),connectivity=1)
+        result = mask.sum()
+    return result
     
 
-def prevent_crash(x,y,action_scores):
+def get_potential(x,y):
+    playfield = get_playfield_centered(x,y) # 12,10
+    return [1+floodfill(12,11, (playfield)),
+            1+floodfill(11,10, (playfield)),
+            1+floodfill(12,9, (playfield)),
+            1+floodfill(13,10, (playfield)),]
+
+def prevent_crash(x,y,action_scores, sample=False):
     playfield = get_playfield()
     actions = [0,0,0,0]
     if playfield[y,(x+24+1)%24] == 0xFF:
@@ -83,16 +99,20 @@ def prevent_crash(x,y,action_scores):
         actions[UP] = 1
     if playfield[(y+20-1)%20,x] == 0xFF:
         actions[DOWN] = 1
-    return int(np.argmax(np.multiply(action_scores, actions)))
+    scores = np.multiply(action_scores, actions)
+    if sample:
+        return int(np.random.choice(np.argwhere(scores == np.amax(scores)).flatten(),1))
+    else:
+        return int(np.argmax(scores))
 
 def eval_genomes(genomes, config):
+    set_graphics(1)
     # sanity check
     for genome_id, genome in genomes:
         genome.fitness = 1
     random.shuffle(genomes)
     n = random.randint(2,7)
     partitioned_genomes = partition(n, genomes)
-    set_graphics(1)
     best_genomes = []
     for part in partitioned_genomes:
         reset()
@@ -100,10 +120,10 @@ def eval_genomes(genomes, config):
         for genome_id, genome in part:
             #nets.append(neat.nn.feed_forward.FeedForwardNetwork.create(genome, config))
             nets.append(neat.nn.recurrent.RecurrentNetwork.create(genome, config))
-            register_player(str(int(genome_id / 1000)))
+            register_player(str(int(genome_id / 100)))
+        register_player("rdfx")
         register_player("flix")
-        register_player("rd2")
-        register_player("rd3")
+        register_player("rd")
         op_id = len(part)
         op2_id = op_id + 1
         op3_id = op2_id + 1
@@ -111,22 +131,22 @@ def eval_genomes(genomes, config):
         alive_players = 8
         cheat_scores = np.ones(8)
         strat_1 = [random.random() for i in range(4)]
-        strat_2 = [random.random() for i in range(4)]
         while(alive_players > 1):
             wait_n_ticks(1)
             i = 0
             for x, y, state, lifetime, dead_by in zip(*get_player_stats()):
                 if state != 0:
                     if i == op_id:
-                        set_player_direction(i, prevent_crash(x,y,get_distances(x,y)))
+                        set_player_direction(i, prevent_crash(x,y,get_potential(x,y), True))
                     elif i == op2_id:
-                        set_player_direction(i, prevent_crash(x,y,strat_1))
+                        set_player_direction(i, prevent_crash(x,y,get_potential(x,y)))
                     elif i == op3_id:
-                        set_player_direction(i, prevent_crash(x,y,strat_2))
+                        set_player_direction(i, prevent_crash(x,y,strat_1))
                     else:
                         genome_id, genome = part[i]
-                        #action_scores = nets[i].activate(get_distances(x,y))
-                        action_scores = nets[i].activate(get_playfield_centered(x,y)[8:15,6:13].flatten())
+                        
+                        action_scores = nets[i].activate(np.concatenate((get_distances(x,y),get_potential(x,y))))
+                        #action_scores = nets[i].activate(get_playfield_centered(x,y)[8:15,6:13].flatten())
                         #[9:14,7:12]
                         set_player_direction(i, prevent_crash(x,y,action_scores))
                 i = i + 1
@@ -144,7 +164,7 @@ def eval_genomes(genomes, config):
                 part[dead_by][1].fitness = part[dead_by][1].fitness * 2#float(lifetime) - (cheat_scores[i] * 0.5)**2
             i = i + 1
     # show best
-    return
+    #return
     n = random.randint(2,8)
     sorted_genomes = sorted(genomes, key=lambda x: x[1].fitness, reverse=True)
     set_graphics(1)
@@ -154,7 +174,7 @@ def eval_genomes(genomes, config):
     for genome_id, genome in sorted_genomes[:n]:
         nets.append(neat.nn.recurrent.RecurrentNetwork.create(genome, config))
         #nets.append(neat.nn.feed_forward.FeedForwardNetwork.create(genome, config))
-        register_player(str(int(genome_id/1000)))
+        register_player(str(int(genome_id/100)))
     register_player("flix")
     initialize_game()
     alive_players = 8
@@ -163,11 +183,11 @@ def eval_genomes(genomes, config):
         for x, y, state, lifetime, _ in zip(*get_player_stats()):
             if state != 0:
                 if i == len(nets):
-                    set_player_direction(prevent_crash(get_distances(x,y)))
+                    set_player_direction(i, prevent_crash(x,y,get_potential(x,y)))
                 else:
                     genome_id, genome = sorted_genomes[i]
-                    #action_scores = nets[i].activate(get_distances(x,y))
-                    action_scores = nets[i].activate(get_playfield_centered(x,y)[8:15,6:13].flatten())
+                    action_scores = nets[i].activate(np.concatenate((get_distances(x,y),get_potential(x,y))))
+                    #action_scores = nets[i].activate(get_playfield_centered(x,y)[8:15,6:13].flatten())
                     #action = int(np.argmax(action_scores))
                     action = prevent_crash(x,y,action_scores)
                     set_player_direction(i, action)
@@ -189,26 +209,15 @@ p.add_reporter(stats)
 p.add_reporter(neat.Checkpointer(5))
 
 
-winner = p.run(eval_genomes, 30000)
+winner = p.run(eval_genomes, 10000)
 
-# Display the winning genome.
-print('\nBest genome:\n{!s}'.format(winner))
-
-# Show output of the most fit genome against training data.
-print('\nOutput:')
-winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
-for xi, xo in zip(xor_inputs, xor_outputs):
-    output = winner_net.activate(xi)
-    print("input {!r}, expected output {!r}, got {!r}".format(xi, xo, output))
-    
-node_names = {-1: 'A', -2: 'B', 0: 'A XOR B'}
-visualize.draw_net(config, winner, True, node_names=node_names)
-visualize.draw_net(config, winner, True, node_names=node_names, prune_unused=True)
+visualize.draw_net(config, winner, True)
+visualize.draw_net(config, winner, True, prune_unused=True)
 visualize.plot_stats(stats, ylog=False, view=True)
 visualize.plot_species(stats, view=True)
 
-p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
-p.run(eval_genomes, 10)
+p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-9')
+p.run(eval_genomes, 1)
 
 
 #setup(1,0,1)
